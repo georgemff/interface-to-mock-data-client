aa207d2e52cc0f2c2aaadb3c7766ec76580062d0
Giorgi Adeishvili <adeishvili65@gmail.com>
2024-04-07 22:54:06 +0400

    add custom types comment to editor

--- src/app/features/convert/editor.config.ts
+++ src/app/features/convert/editor.config.ts
@@ -3,7 +3,19 @@ import {CodeModel} from "@ngstack/code-editor";
 export const codeModel: CodeModel = {
   language: 'typescript',
   uri: 'main.ts',
-  value: 'interface T {\n\n}'
+  value: `
+/*
+  You can use custom types
+
+  CustomTypes.FIRST_NAME
+  CustomTypes.LAST_NAME
+  CustomTypes.NICK_NAME
+  CustomTypes.EMAIL
+  CustomTypes.DESCRIPTION
+    
+*/
+
+interface I {\n\n}`
 };
 
 export const options = {
